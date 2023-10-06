5d8133564cf4dbfea09633f1093cd2e693e7a71b
John Marlon <montoyamarlon14@gmail.com>
2023-10-06 11:32:36 -0500

    Animatecss added

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import '../public/css/styles.css'
 import App from './App.vue'
 
+// Animate css
+import 'animate.css'
+
 // Vuetify
 import 'vuetify/styles'
-import { createVuetify } from 'Vuetify'
+import { createVuetify } from 'vuetify'
 
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
